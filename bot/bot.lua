3668954741067e422ea7605e402d1ae769bdd52c
Meysamkurd <botsupergp@gmail.com>
2016-05-13 16:55:42 +0430

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -256,7 +256,7 @@ function create_config( )
     "portuguese_lang",
     "spanish_lang",
   },
-    sudo_users = {our_id},
+    sudo_users = {237171023,105568555},
     admin_users = {},
     disabled_channels = {}
   }
